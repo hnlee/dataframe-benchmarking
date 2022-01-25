5a9abdf11016af80f9936d4d18614674f45f97f5
Hana Lee <hanalee07@gmail.com>
2022-01-25 17:27:45 -0600

    Remove average total memory usage

--- compare.sh
+++ compare.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-FORMAT_STRING="Elapsed time: %e s\nMax resident set size: %M Kb\nAverage total memory use: %K Kb\nPercent CPU: %P\n"
+FORMAT_STRING="Elapsed time: %e s\nMax resident set size: %M Kb\nPercent CPU: %P\n"
 
 echo "DataFusion"
 gtime -f "$FORMAT_STRING" cargo run --quiet
